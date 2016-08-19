6733034ea22d6cccc1965e3c36ab9d7cc3571e47
Graham Martin <graham@pixely.co.uk>
2016-08-19 12:51:57 +0100

    Add babel to compile JavaScript

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,23 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+
+gulp.task('script', () => {
+  gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat('details-polyfill.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('lint', () => {
-  gulp.src(['**/*.js', '!node_modules/**'])
+  gulp.src(['**/*.js', '!node_modules/**', '!dist/*.js'])
       .pipe(eslint())
       .pipe(eslint.format())
       .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['lint']);
+gulp.task('default', ['lint', 'script']);
